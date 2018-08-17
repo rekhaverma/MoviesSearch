8d286ebaaf2aa6a25345f893679c8e6338bfbbac
rekhaverma <rekhavermakuk56@gmail.com>
2018-08-17 13:56:26 +0530

    Removed unused components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 // All the required components needs to place here://
-import Input from "./input";
 import SearchInput from "./searchInput";
-import Icon from './icon';
 import Loading from "./loading";
 
-export { Input, Icon, SearchInput, Loading };
+export { SearchInput, Loading };
